c5e54c8a2405027af3cff9e63ae8123cdb4fc9ed
ayrtongomes070-pixel <ayrtongomes070@gmail.com>
2026-09-24 16:18:56 -0300

    segundo commit, fiz mais algumas linhas de código, ajeitei algumas coisas em relação ao parser.c com o token.h e estruturei o código até o momento de forma mais enxuta

--- micropascal-compiler/src/parser.c
+++ micropascal-compiler/src/parser.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/parser.h"
-
-static parser_avancar(Parser *parser);
+ 
+static void parser_avancar(Parser *parser);
 static void parser_erro(Parser *parser);
-static void parser_esperra(Parser *parser, TipoToken tipo);
-
+static void parser_esperar(Parser *parser, TipoToken tipo);
+static int token_em(TipoToken tipo, const TipoToken *conjunto, int n);
+ 
 static void parser_secao_var(Parser *parser);
 static void parser_decl_var(Parser *parser);
 static void parser_tipo(Parser *parser);
@@ -16,43 +17,82 @@ static void parser_atribuicao(Parser *parser);
 static void parser_iteracao(Parser *parser);
 static void parser_decisao(Parser *parser);
 static void parser_escrita(Parser *parser);
-
+ 
 static void parser_expressao(Parser *parser);
 static void parser_relacional(Parser *parser);
 static void parser_soma(Parser *parser);
 static void parser_termo(Parser *parser);
 static void parser_fator(Parser *parser);
-
-static void parser_avancar(Parser *parser) {
-    parser ->token_atual = lexer_proximo_token (parser->lexer);
+ 
+static const TipoToken INICIO_COMANDO[] = {
+    TOKEN_BEGIN, TOKEN_IDENTIFICADOR, TOKEN_WHILE, TOKEN_REPEAT, TOKEN_IF, TOKEN_WRITE
+};
+static const TipoToken OP_OR_AND[] = { TOKEN_OR, TOKEN_AND };
+static const TipoToken OP_RELACIONAL[] = {
+    TOKEN_IGUAL, TOKEN_DIFERENTE, TOKEN_MENOR, TOKEN_MAIOR, TOKEN_MENOR_IGUAL, TOKEN_MAIOR_IGUAL
+};
+static const TipoToken OP_SOMA[] = { TOKEN_MAIS, TOKEN_MENOS };
+static const TipoToken OP_TERMO[] = { TOKEN_MULT, TOKEN_DIV_REAL, TOKEN_DIV_INT };
+ 
+static int token_em(TipoToken tipo, const TipoToken *conjunto, int n) {
+    for (int i = 0; i < n; i++) {
+        if (conjunto[i] == tipo) return 1;
+    }
+    return 0;
 }
-
-static void parser_erro(Parser *parser){
+ 
+static void parser_avancar(Parser *parser) {
+    parser->token_atual = lexer_proximo_token(parser->lexer);
+}
+ 
+static void parser_erro(Parser *parser) {
     fprintf(stderr, "Erro de sintaxe no token %s\n", parser->token_atual.lexema);
     exit(1);
 }
-
+ 
 static void parser_esperar(Parser *parser, TipoToken tipo) {
-    if (parser->token_atual.tipo == tipo){
-        parser_avancar(parser);
-    } else {
-        parser_erro(parser);
-    }
+    if (parser->token_atual.tipo != tipo) parser_erro(parser);
+    parser_avancar(parser);
 }
-
-void parser_analisar_programa(Parser *parser ){
+ 
+void parser_inicializar(Parser *parser, Lexer *lexer) {
+    parser->lexer = lexer;
+    parser_avancar(parser);
+}
+ 
+void parser_analisar_programa(Parser *parser) {
     parser_esperar(parser, TOKEN_PROGRAM);
     parser_esperar(parser, TOKEN_IDENTIFICADOR);
-    parser_esperar(parser, TOKEN_PONTO_VIRGULA);
+    parser_esperar(parser, TOKEN_PONTOVIRG);
     parser_secao_var(parser);
     parser_bloco(parser);
     parser_esperar(parser, TOKEN_PONTO);
 }
-
+ 
 static void parser_secao_var(Parser *parser) {
     parser_esperar(parser, TOKEN_VAR);
-    while (parser->token_atual.tipo == TOKEN_IDENTIFICADOR){
-        parser_decl_var(parser);
+    while (parser->token_atual.tipo == TOKEN_IDENTIFICADOR) parser_decl_var(parser);
+}
+ 
+static void parser_decl_var(Parser *parser) {
+    parser_esperar(parser, TOKEN_IDENTIFICADOR);
+    while (parser->token_atual.tipo == TOKEN_VIRGULA) {
+        parser_avancar(parser);
+        parser_esperar(parser, TOKEN_IDENTIFICADOR);
     }
+    parser_esperar(parser, TOKEN_DOISPONTOS);
+    parser_tipo(parser);
+    parser_esperar(parser, TOKEN_PONTOVIRG);
 }
 
+static void parser_tipo(Parser *parser){
+    TipoToken t = parser->token_atual.tipo;
+    if (t != TOKEN_INTEGER && t!= TOKEN_REAL && t!= TOKEN_CHAR) parser_erro(parser);
+    parser_avancar(parser);
+}
+
+static void parser_bloco(Parser *parser){
+    parser_esperar(parser, TOKEN_BEGIN);
+    parser_lista_comandos(parser);
+    parser_esperar(parser, TOKEN_END);
+}
