8f978eecceef7832dc3aaa31f18953a4a48f9066
ayrtongomes070-pixel <ayrtongomes070@gmail.com>
2026-09-23 23:04:35 -0300

    commit inicial do arquivo parser.h e parser.c

--- micropascal-compiler/src/parser.c
+++ micropascal-compiler/src/parser.c
@@ -2,35 +2,57 @@
 #include <stdlib.h>
 #include "../include/parser.h"
 
-/*
- * parser.c
- *
- * Implementação do analisador sintático do micro-Pascal.
- *
- * TODO:
- *   - Implementar <programa>, <secao_var>, <decl_var>, <tipo>
- *   - Implementar <bloco>, <lista_comandos>, <comando>
- *   - Implementar <atribuicao>, <iteracao>, <decisao>, <escrita>
- *   - Implementar <expressao> e <expr_basica> respeitando a precedência:
- *       1. *, /, div
- *       2. +, -
- *       3. =, <>, <, >, <=, >=
- *       4. or, and
- *     (todos associativos à esquerda; atenção à recursão à esquerda
- *      da gramática abstrata original, que precisa ser eliminada)
- *   - Emitir erro no formato: "Erro de sintaxe no token [lexema]"
- */
+static parser_avancar(Parser *parser);
+static void parser_erro(Parser *parser);
+static void parser_esperra(Parser *parser, TipoToken tipo);
+
+static void parser_secao_var(Parser *parser);
+static void parser_decl_var(Parser *parser);
+static void parser_tipo(Parser *parser);
+static void parser_bloco(Parser *parser);
+static void parser_lista_comandos(Parser *parser);
+static void parser_comando(Parser *parser);
+static void parser_atribuicao(Parser *parser);
+static void parser_iteracao(Parser *parser);
+static void parser_decisao(Parser *parser);
+static void parser_escrita(Parser *parser);
+
+static void parser_expressao(Parser *parser);
+static void parser_relacional(Parser *parser);
+static void parser_soma(Parser *parser);
+static void parser_termo(Parser *parser);
+static void parser_fator(Parser *parser);
 
 static void parser_avancar(Parser *parser) {
-    parser->token_atual = lexer_proximo_token(parser->lexer);
+    parser ->token_atual = lexer_proximo_token (parser->lexer);
 }
 
-void parser_inicializar(Parser *parser, Lexer *lexer) {
-    parser->lexer = lexer;
-    parser_avancar(parser);
+static void parser_erro(Parser *parser){
+    fprintf(stderr, "Erro de sintaxe no token %s\n", parser->token_atual.lexema);
+    exit(1);
 }
 
-void parser_analisar_programa(Parser *parser) {
-    /* TODO: implementar <programa> ::= program IDENTIFICADOR ; <secao_var> <bloco> . */
-    (void)parser;
+static void parser_esperar(Parser *parser, TipoToken tipo) {
+    if (parser->token_atual.tipo == tipo){
+        parser_avancar(parser);
+    } else {
+        parser_erro(parser);
+    }
 }
+
+void parser_analisar_programa(Parser *parser ){
+    parser_esperar(parser, TOKEN_PROGRAM);
+    parser_esperar(parser, TOKEN_IDENTIFICADOR);
+    parser_esperar(parser, TOKEN_PONTO_VIRGULA);
+    parser_secao_var(parser);
+    parser_bloco(parser);
+    parser_esperar(parser, TOKEN_PONTO);
+}
+
+static void parser_secao_var(Parser *parser) {
+    parser_esperar(parser, TOKEN_VAR);
+    while (parser->token_atual.tipo == TOKEN_IDENTIFICADOR){
+        parser_decl_var(parser);
+    }
+}
+
